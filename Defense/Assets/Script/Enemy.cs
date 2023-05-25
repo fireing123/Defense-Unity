8ae9dd3f4811f215c8565fbb3c32da5199fbd518
fireing123 <88523379+fireing123@users.noreply.github.com>
2023-05-25 12:24:53 +0900

    mmm

--- Defense/Assets/Script/Enemy.cs
+++ Defense/Assets/Script/Enemy.cs
@@ -1,62 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    public static int speed = 366;
-    public Transform[] load;
-    public int next = 2;
-    private Vector3 Move;
-    private List<Vector3> positionList = new List<Vector3>();
-    // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
-        var Lo = GameObject.Find("load");
-        load = Lo.GetComponentsInChildren<Transform>();
-        foreach (Transform t in load)
-        {
-
-            positionList.Add(t.position);
-        }
-        var Direction = GetDirection(positionList[next -1], positionList[next]);
-        var MoveSpeed = GetMoveSpeed(Direction, speed);
-        Move = MoveSpeed;
+        var LoadManger = GameObject.Find("Load").GetComponent<Load>();
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        if (load.Length == next) { Debug.Log("hello"); return; }
-        Moving();
-    }
-    
-    private void Moving()
-    {
-        transform.Translate(Move);
-        var myposition = GetPosition(gameObject.transform);
-        var myendposition = positionList[next];
-        if (isArrived(GetIntVector(myposition), GetIntVector(myendposition)))
-        {
-            if (next == load.Length) return;
-            gameObject.transform.position = positionList[next];
-            next += 1;
-            var Direction = GetDirection(positionList[next - 1], positionList[next]);
-            var MoveSpeed = GetMoveSpeed(Direction, speed);
-            Move = MoveSpeed;
-        }
-    }
-
-    private Vector3 GetPosition(Transform gameObject) => gameObject.position;
-
-    private Vector3 GetDirection(Vector3 a, Vector3 b) => b - a;
-
-    private Vector3 GetMoveSpeed(Vector3 v, int s) => new Vector3(v.x/s, v.y/s, v.z/s);
-
-    private Vector3 GetTimeMulty(Vector3 v) => v * Time.deltaTime;
-
-    private Vector3Int GetIntVector(Vector3 v) => new Vector3Int((int)v.x, (int)v.y, (int)v.z);
-
-    private bool isArrived(Vector3Int a, Vector3Int b) => a == b;
 }
 
--- Defense/Assets/Script/Load.cs
+++ Defense/Assets/Script/Load.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Load : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public int IntLoad;
+    public Vector3[] LoadUnits;
+    public Vector3[] Course;
+
+    void Awake()
     {
-        
+        Transform[] LoadTransform = GetComponentsInChildren<Transform>();
+        int LoadTransformLength = LoadTransform.Length;
+        Vector3[] vector3s = new Vector3[LoadTransformLength];
+        for(int i =0; i < LoadTransformLength; i++)
+        {
+            vector3s[i] = LoadTransform[i].position;
+        }
+        LoadUnits = vector3s;
+        IntLoad = LoadUnits.Length;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    private void GetCheckingWay()
+    {
+
     }
 }
