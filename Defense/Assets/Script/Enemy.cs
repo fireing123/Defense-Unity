c1f7ec369974fa19a70a9c9dc0d87c726caf0795
fireing123 <88523379+fireing123@users.noreply.github.com>
2023-05-29 15:37:17 +0900

    finish? load

--- Defense/Assets/Script/Enemy.cs
+++ Defense/Assets/Script/Enemy.cs
@@ -1,13 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+
+    public Vector3 nextDirection;
+    public int speed;
+
     void Start()
     {
-        var LoadManger = GameObject.Find("Load").GetComponent<Load>();
+    
+    }
+
+    private void Update()
+    {
+        transform.Translate(nextDirection * speed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("gfgf");
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("gg");
+        if (other.tag != "load") return;
+        transform.position = other.transform.position;
+        nextDirection = - other.GetComponent<Load>().nodeDirection / 360;
     }
 
 }
--- Defense/Assets/Script/Load.cs
+++ Defense/Assets/Script/Load.cs
@@ -5,29 +5,18 @@ using UnityEngine;
 public class Load : MonoBehaviour
 {
 
-    public int IntLoad;
-    public Vector3[] LoadUnits;
-    public Vector3[] Course;
+    public Transform nextNode;
+
+    public Vector3 nodeDirection;
 
     void Awake()
     {
-        Transform[] LoadTransform = GetComponentsInChildren<Transform>();
-        int LoadTransformLength = LoadTransform.Length;
-        Vector3[] vector3s = new Vector3[LoadTransformLength];
-        for(int i =0; i < LoadTransformLength; i++)
-        {
-            vector3s[i] = LoadTransform[i].position;
-        }
-        LoadUnits = vector3s;
-        IntLoad = LoadUnits.Length;
+        gameObject.tag = "load";
+        var Direction = transform.position - nextNode.position;
+        nodeDirection = Direction / Vector3.Distance(transform.position , nextNode.position);
     }
 
     void Update()
     {
-    }
-
-    private void GetCheckingWay()
-    {
-
     }
 }
